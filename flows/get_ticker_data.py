be52a02c185218b9a9d5b5707f734c0fada8caee
Eli Lee Kitagawa <cosmosart@msn.com>
2026-03-18 17:18:34 +0900

    data insert test

--- flows/get_ticker_data.py
+++ flows/get_ticker_data.py
@@ -14,43 +14,42 @@ def fetch_price_data(ticker):
     data = yf.download(ticker, period="5d")
     if data.empty:
         return None
-    row = data
+    row = data.iloc[0]
     logger.info(str(row))
-    # return {
-    #     "ticker": ticker,
-    #     "date": row.name.date(),
-    #     "open": row["Open"],
-    #     "high": row["High"],
-    #     "low": row["Low"],
-    #     "close": row["Close"],
-    #     "adj_close": row.get("Adj Close", row["Close"]),
-    #     "volume": int(row["Volume"]),
-    #     "region": "US"
-    # }
-    return None
+    return {
+        "ticker": ticker,
+        "date": row.name.date(),
+        "open": row["Open"],
+        "high": row["High"],
+        "low": row["Low"],
+        "close": row["Close"],
+        "adj_close": row.get("Adj Close", row["Close"]),
+        "volume": int(row["Volume"]),
+        "region": "US"
+    }
 
 @task
 def save_to_db(price_data):
     if not price_data:
         return
-    # with get_connection() as conn, conn.cursor() as cur:
-    #     cur.execute("""
-    #             INSERT INTO price_daily (ticker, region, date, open, high, low, close, adj_close, volume)
-    #             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-    #             ON CONFLICT (ticker, date) DO NOTHING;
-    # """, (
-    #     price_data["ticker"],
-    #     price_data["region"],
-    #     price_data["date"],
-    #     price_data["open"],
-    #     price_data["high"],
-    #     price_data["low"],
-    #     price_data["close"],
-    #     price_data["adj_close"],
-    #     price_data["volume"]
-    # ))
-    # conn.commit()
-    check_health()  # Ensure DB is healthy before attempting to save
+    with get_connection() as conn, conn.cursor() as cur:
+        cur.execute("""
+                INSERT INTO price_daily (ticker, region, date, open, high, low, close, adj_close, volume)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (ticker, date) DO NOTHING;
+    """, (
+        price_data["ticker"],
+        price_data["region"],
+        price_data["date"],
+        price_data["open"],
+        price_data["high"],
+        price_data["low"],
+        price_data["close"],
+        price_data["adj_close"],
+        price_data["volume"]
+    ))
+    conn.commit()
+
 
 @flow
 def daily_batch():
